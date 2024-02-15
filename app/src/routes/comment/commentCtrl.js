05715cb09fa673f3850a8def4a046c6810159066
cyoure <wwj9559@gmail.com>
2024-02-15 15:32:14 +0900

    modify/#14: 유효성 검사 기능 추가

--- app/src/routes/comment/commentCtrl.js
+++ app/src/routes/comment/commentCtrl.js
@@ -1,19 +1,38 @@
 "use strict";
 
 import CommentService from "../../models/comment/commentService.js";
+import {validationResult} from "express-validator";
 
 const process = {
   addComments: async (req, res) => {
+    const error = validationResult(req).errors[0];
+    if (error) {
+      return res
+        .status(400)
+        .json({error: "Bad Request", message: error.msg, Statuscode: 400});
+    }
     const comment = new CommentService(req);
     const response = await comment.addComments();
     return res.json(response);
   },
   deleteComments: async (req, res) => {
+    const error = validationResult(req).errors[0];
+    if (error) {
+      return res
+        .status(400)
+        .json({error: "Bad Request", message: error.msg, Statuscode: 400});
+    }
     const comment = new CommentService(req);
     const response = await comment.deleteComments();
     return res.json(response);
   },
   getComments: async (req, res) => {
+    const error = validationResult(req).errors[0];
+    if (error) {
+      return res
+        .status(400)
+        .json({error: "Bad Request", message: error.msg, Statuscode: 400});
+    }
     const comment = new CommentService(req);
     const response = await comment.getComments();
     return res.json(response);
--- app/src/routes/comment/commentRouters.js
+++ app/src/routes/comment/commentRouters.js
@@ -2,11 +2,42 @@
 
 import express from "express";
 import ctrl from "./commentCtrl.js";
+import {param, body} from "express-validator";
 
 const router = express.Router();
 
-router.post("/:board_no", ctrl.process.addComments);
-router.get("/", ctrl.process.getComments);
-router.delete("/", ctrl.process.deleteComments);
+router.post(
+  "/:board_no",
+  param("board_no")
+    .notEmpty()
+    .withMessage("게시글 번호는 비어있을 수 없습니다.")
+    .isInt()
+    .withMessage("게시글 번호는 정수이어야 합니다."),
+  body("user_no")
+    .notEmpty()
+    .withMessage("유저 번호는 비어있을 수 없습니다.")
+    .isInt()
+    .withMessage("유저 번호는 정수이어야 합니다."),
+  body("content").notEmpty().withMessage("댓글은 비어있을 수 없습니다."),
+  ctrl.process.addComments
+);
+router.get(
+  "/",
+  body("board_no")
+    .notEmpty()
+    .withMessage("게시글 번호는 비어있을 수 없습니다.")
+    .isInt()
+    .withMessage("게시글 번호는 정수이어야 합니다."),
+  ctrl.process.getComments
+);
+router.delete(
+  "/",
+  body("no")
+    .notEmpty()
+    .withMessage("댓글 고유 번호는 비어있을 수 없습니다.")
+    .isInt()
+    .withMessage("댓글 고유 번호는 정수이어야 합니다."),
+  ctrl.process.deleteComments
+);
 
 export default router;
