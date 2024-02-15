0ff77c4239d8f7855ed051784b1ebdacde395430
JYE <jo1814@naver.com>
2024-02-15 11:42:53 +0900

    feature/#13: 메인페이지 조회 유효성 검사 구현 및 validatioin관련 새로운 파일을 위한 파일생성

--- app/src/routes/mainpage/mainpageCtrl.js
+++ app/src/routes/mainpage/mainpageCtrl.js
@@ -1,4 +1,6 @@
 import MainpageService from "../../models/mainpage/mainpageService.js";
+import {validationResult} from "express-validator";
+
 export default {
   process: {
     getUser: async (req, res) => {
@@ -10,6 +12,14 @@ export default {
     },
 
     getBoardsAndLoveCountAndCommentCount: async (req, res) => {
+      const error = validationResult(req).errors[0];
+
+      if (error) {
+        return res
+          .status(400)
+          .json({error: "Bad Request", messgae: error.msg, statusCode: 400});
+      }
+
       const instance = new MainpageService(req);
 
       const response = await instance.getBoardsAndLoveCountAndCommentCount();
