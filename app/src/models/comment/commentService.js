3efe3bcca1574ec99636b96a056250eaeae0676c
cyoure <wwj9559@gmail.com>
2024-02-15 17:07:56 +0900

    modify/#14: 데이터베이스 값 확인 절차 추가

--- app/src/models/comment/commentService.js
+++ app/src/models/comment/commentService.js
@@ -11,34 +11,52 @@ class CommentService {
     const boardNo = this.params.board_no;
     const userNo = this.body.user_no;
     const comments = this.body.content;
-    try {
-      const response = await CommentRepository.addComments(
-        boardNo,
-        userNo,
-        comments
-      );
-      return response;
-    } catch (err) {
-      return {success: false};
+
+    let error = await CommentRepository.isBoardNO(boardNo);
+    if (!error[0][0]) {
+      return {
+        error: "Not Found",
+        message: "해당 게시글이 존재하지 않습니다.",
+        Statuscode: 404,
+      };
     }
+    error = await CommentRepository.isUserNO(userNo);
+    if (!error[0][0]) {
+      return {
+        error: "Not Found",
+        message: "해당 유저가 존재하지 않습니다.",
+        Statuscode: 404,
+      };
+    }
+
+    await CommentRepository.addComments(boardNo, userNo, comments);
+    return {Statuscode: 201};
   }
   async deleteComments() {
     const No = this.body.no;
-    try {
-      const response = await CommentRepository.deleteComments(No);
-      return response;
-    } catch (err) {
-      return {success: false};
+    let error = await CommentRepository.isNO(No);
+    if (!error[0][0]) {
+      return {
+        error: "Not Found",
+        message: "해당 댓글이 존재하지 않습니다.",
+        Statuscode: 404,
+      };
     }
+    await CommentRepository.deleteComments(No);
+    return {message: "댓글이 성공적으로 삭제됐습니다.", Status: 200};
   }
   async getComments() {
     const boardNo = this.body.board_no;
-    try {
-      const [rows, fields] = await CommentRepository.getComments(boardNo);
-      return rows;
-    } catch {
-      return {success: false};
+    let error = await CommentRepository.isBoardNO(boardNo);
+    if (!error[0][0]) {
+      return {
+        error: "Not Found",
+        message: "해당 게시글이 존재하지 않습니다.",
+        statuscode: 404,
+      };
     }
+    const [rows, fields] = await CommentRepository.getComments(boardNo);
+    return {Statuscode: 201, rows};
   }
 }
 
--- app/src/models/comment/commnetRepository.js
+++ app/src/models/comment/commnetRepository.js
@@ -8,14 +8,31 @@ class CommentRepository {
       "insert into comment(board_no, user_no, content) values(?, ?, ?);";
     return db.query(query, [boardNo, userNo, comments]);
   }
+
   static deleteComments(No) {
     const query = "delete from comment where no = ?;";
     return db.query(query, [No]);
   }
+
   static getComments(boardNo) {
     const query =
       "select * , (select count(*) from comment where board_no = ?) as comment_count from comment where board_no =?;";
     return db.query(query, [boardNo, boardNo]);
+  }
+
+  static isUserNO(userNo) {
+    const query = "select no from user where no = ?;";
+    return db.query(query, [userNo]);
+  }
+
+  static isNO(No) {
+    const query = "select no from comment where no = ?;";
+    return db.query(query, [No]);
+  }
+
+  static isBoardNO(boardNo) {
+    const query = "select no from board where no = ?;";
+    return db.query(query, [boardNo]);
   }
 }
 
