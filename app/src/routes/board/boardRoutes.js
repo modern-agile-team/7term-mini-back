93540b6802c085d2e357fc70b43062471adf5efa
JYE <jo1814@naver.com>
2024-02-16 11:28:12 +0900

    refactor/#6: validation 파일 정리

--- app/src/routes/board/boardRoutes.js
+++ app/src/routes/board/boardRoutes.js
@@ -1,70 +1,31 @@
 import express from "express";
 import ctrl from "./boardCtrl.js";
 import {body, param, header} from "express-validator";
+import validation from "./boardValidation.js";
 
 const router = express.Router();
 
 //보드 생성
-router.post(
-  "/",
-  header("user_no")
-    .notEmpty()
-    .withMessage("유저 고유번호가 필요합니다.")
-    .isInt({min: 1})
-    .withMessage("유저 고유번호는 자연수여야 합니다."),
-  body("categoryNo")
-    .notEmpty()
-    .withMessage("카테고리 번호는 비어둘 수 없습니다.")
-    .isInt({min: 1})
-    .withMessage("카테고리 번호는 자연수여야 합니다."),
-  body("content")
-    .notEmpty()
-    .withMessage("내용을 입력해주세요")
-    .isLength({max: 250})
-    .withMessage("내용은 250자 이내여야 합니다."),
-  ctrl.process.appendBoard
-);
+router.post("/", validation.process.checkBeforePost, ctrl.process.appendBoard);
 
 //보드 삭제
 router.delete(
   "/:boardNo",
-  param("boardNo")
-    .notEmpty()
-    .withMessage("삭제할 게시글의 번호가 필요합니다.")
-    .isInt({min: 1})
-    .withMessage("삭제할 게시글의 번호는 자연수여야 합니다."),
+  validation.process.checkBeforeDelete,
   ctrl.process.deleteBoard
 );
 
 //보드 조회
 router.get(
   "/:boardNo",
-  param("boardNo")
-    .notEmpty()
-    .withMessage("조회할 게시글의 번호가 필요합니다.")
-    .isInt({min: 1})
-    .withMessage("조회할 게시글의 번호는 자연수여야 합니다."),
+  validation.process.checkBeforeGet,
   ctrl.process.findOneBoardWithNicknameAndLoveCount
 );
 
 //보드 수정
 router.put(
   "/:boardNo",
-  param("boardNo")
-    .notEmpty()
-    .withMessage("조회할 게시글의 번호가 필요합니다.")
-    .isInt({min: 1})
-    .withMessage("조회할 게시글의 번호는 자연수여야 합니다."),
-  body("categoryNo")
-    .notEmpty()
-    .withMessage("카테고리 번호는 비어둘 수 없습니다.")
-    .isInt({min: 1})
-    .withMessage("카테고리 번호는 자연수를 입력해야합니다."),
-  body("content")
-    .notEmpty()
-    .withMessage("내용을 입력해주세요.")
-    .isLength({max: 250})
-    .withMessage("내용은 250자 이내여야 합니다."),
+  validation.process.checkBeforePut,
   ctrl.process.updateBoard
 );
 
---
+++ app/src/routes/board/boardValidation.js
@@ -0,0 +1,57 @@
+import {header, body, param} from "express-validator";
+
+export default {
+  process: {
+    checkBeforePost: [
+      header("user_no")
+        .notEmpty()
+        .withMessage("유저 고유번호가 필요합니다.")
+        .isInt({min: 1})
+        .withMessage("유저 고유번호는 자연수여야 합니다."),
+      body("categoryNo")
+        .notEmpty()
+        .withMessage("카테고리 번호는 비어둘 수 없습니다.")
+        .isInt({min: 1})
+        .withMessage("카테고리 번호는 자연수여야 합니다."),
+      body("content")
+        .notEmpty()
+        .withMessage("내용을 입력해주세요")
+        .isLength({max: 250})
+        .withMessage("내용은 250자 이내여야 합니다."),
+    ],
+
+    checkBeforeDelete: [
+      param("boardNo")
+        .notEmpty()
+        .withMessage("삭제할 게시글의 번호가 필요합니다.")
+        .isInt({min: 1})
+        .withMessage("삭제할 게시글의 번호는 자연수여야 합니다."),
+    ],
+
+    checkBeforeGet: [
+      param("boardNo")
+        .notEmpty()
+        .withMessage("조회할 게시글의 번호가 필요합니다.")
+        .isInt({min: 1})
+        .withMessage("조회할 게시글의 번호는 자연수여야 합니다."),
+    ],
+
+    checkBeforePut: [
+      param("boardNo")
+        .notEmpty()
+        .withMessage("조회할 게시글의 번호가 필요합니다.")
+        .isInt({min: 1})
+        .withMessage("조회할 게시글의 번호는 자연수여야 합니다."),
+      body("categoryNo")
+        .notEmpty()
+        .withMessage("카테고리 번호는 비어둘 수 없습니다.")
+        .isInt({min: 1})
+        .withMessage("카테고리 번호는 자연수를 입력해야합니다."),
+      body("content")
+        .notEmpty()
+        .withMessage("내용을 입력해주세요.")
+        .isLength({max: 250})
+        .withMessage("내용은 250자 이내여야 합니다."),
+    ],
+  },
+};
