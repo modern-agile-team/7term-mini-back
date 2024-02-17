f9721881af08ee842447ba84f22bb16fb87705b2
JYE <jo1814@naver.com>
2024-02-17 18:22:54 +0900

    refactor/#13: 메인페이지 validation 개편

--- app/src/routes/mainpage/mainpageCtrl.js
+++ app/src/routes/mainpage/mainpageCtrl.js
@@ -1,17 +1,8 @@
 import MainpageService from "../../models/mainpage/mainpageService.js";
-import {validationResult} from "express-validator";
 
 export default {
   process: {
     getUser: async (req, res) => {
-      const error = validationResult(req).errors[0];
-
-      if (error) {
-        return res
-          .status(400)
-          .json({error: "Bad Request", messgae: error.msg, statusCode: 400});
-      }
-
       const instance = new MainpageService(req);
 
       const response = await instance.getUserName();
@@ -20,14 +11,6 @@ export default {
     },
 
     getBoardsAndLoveCountAndCommentCount: async (req, res) => {
-      const error = validationResult(req).errors[0];
-
-      if (error) {
-        return res
-          .status(400)
-          .json({error: "Bad Request", messgae: error.msg, statusCode: 400});
-      }
-
       const instance = new MainpageService(req);
 
       const response = await instance.getBoardsAndLoveCountAndCommentCount();
--- app/src/routes/mainpage/mainpageValidation.js
+++ app/src/routes/mainpage/mainpageValidation.js
@@ -1,23 +1,38 @@
-import {header, param, query} from "express-validator";
+import {param, query, validationResult} from "express-validator";
 
 export default {
   process: {
-    checkBoards: [
-      param("currentPageNumber")
+    checkBoards: async (req, res, next) => {
+      await param("currentPageNumber")
         .notEmpty()
         .withMessage("현재 페이지 값은 비워둘 수 없습니다.")
         .isInt({min: 1})
-        .withMessage("현재 페이지 값은 자연수만 들어가야합니다."),
-      query("categoryNo")
+        .withMessage("현재 페이지 값은 자연수만 들어가야합니다.")
+        .run(req);
+
+      await query("categoryNo")
         .notEmpty()
         .withMessage("카테고리 번호는 비워둘 수 없습니다.")
         .isInt({min: 0})
-        .withMessage("카테고리 번호는 0, 자연수만 들어가야합니다."),
-      query("numberBoardsYouWant")
+        .withMessage("카테고리 번호는 0, 자연수만 들어가야합니다.")
+        .run(req);
+
+      await query("numberBoardsYouWant")
         .notEmpty()
         .withMessage("선택할 페이지는 빈값이면 안됩니다.")
         .isInt({min: 1})
-        .withMessage("선택할 페이지는 자연수만 들어가야합니다."),
-    ],
+        .withMessage("선택할 페이지는 자연수만 들어가야합니다.")
+        .run(req);
+
+      const error = validationResult(req).errors[0];
+
+      if (error) {
+        return res
+          .status(400)
+          .json({error: "Bad Request", message: error.msg, statusCode: 400});
+      }
+
+      next();
+    },
   },
 };
