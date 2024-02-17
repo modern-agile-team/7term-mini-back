e36d9f514a2270fe44324b3d8dd38728746373f8
JYE <jo1814@naver.com>
2024-02-17 17:35:26 +0900

    feature/#4: validation 파일 개편 및 조건 추가

--- app/src/routes/user/userCtrl.js
+++ app/src/routes/user/userCtrl.js
@@ -1,19 +1,9 @@
 "use strict";
 
 import User from "../../models/user/userService.js";
-import {validationResult} from "express-validator";
 
 const process = {
   createUser: async (req, res) => {
-    console.log(validationResult(req).errors);
-    const error = validationResult(req).errors;
-
-    if (error[0]) {
-      return res
-        .status(400)
-        .json({error: "Bad Request", message: error[0].msg, statusCode: 400});
-    }
-
     const user = new User(req);
     const response = await user.createUser();
 
--- app/src/routes/user/userValidation.js
+++ app/src/routes/user/userValidation.js
@@ -1,32 +1,64 @@
-import {body} from "express-validator";
+import {validationResult, body} from "express-validator";
 
 export default {
   process: {
-    check: [
-      body("id")
+    check: async (req, res, next) => {
+      await body("id")
         .notEmpty()
         .withMessage("ID는 빈값이 들어올 수 없습니다.")
+        .bail()
         .isAlphanumeric()
-        .withMessage("ID는 알파벳, 숫자만 사용할 수 있습니다.")
+        .withMessage(
+          "ID는 알파벳, 숫자만 사용할 수 있습니다. 공백은 허용되지 않습니다."
+        )
+        .bail()
         .isLength({max: 15})
-        .withMessage("아이디는 15자 이하여야만 가능합니다."),
-      body("nickname")
+        .withMessage("아이디는 15자 이하여야만 가능합니다.")
+        .run(req);
+
+      await body("nickname")
         .notEmpty()
         .withMessage("닉네임은 빈값이 들어올 수 없습니다.")
-        .isLength({max: 12})
-        .withMessage("닉네임은 12자 이하여야만 가능합니다."),
-      body("password")
+        .bail()
+        .isLength({max: 5})
+        .withMessage("닉네임은 5자 이하여야만 가능합니다.")
+        .bail()
+        .matches(/^ㄱ-힣a-zA-Z0-9/) //이 부분은 바뀔 가능성 농후
+        .withMessage(
+          "닉네임은 한글, 영어, 숫자로만 이루어져야 합니다. 공백은 허용되지 않습니다."
+        )
+        .run(req);
+
+      await body("password")
         .notEmpty()
         .withMessage("패스워드는 빈값이 들어올 수 없습니다.")
+        .bail()
         .isLength({max: 20})
-        .withMessage("패스워드는 20자 이하여야만 가능합니다."),
-      body("email")
+        .withMessage("패스워드는 20자 이하여야만 가능합니다.")
+        .run(req);
+
+      await body("email")
         .notEmpty()
         .withMessage("이메일은 빈값이 들어올 수 없습니다.")
+        .bail()
         .isEmail({allow_utf8_local_part: false})
         .withMessage("이메일은 숫자와 영어만 허용돱니다.")
+        .bail()
         .isLength({max: 30})
-        .withMessage("이메일은 30자 이하여야만 가능합니다."),
-    ],
+        .withMessage("이메일은 30자 이하여야만 가능합니다.")
+        .run(req);
+
+      const error = validationResult(req).errors[0];
+
+      if (error) {
+        return res.status(400).json({
+          error: "Bad Request",
+          message: error.msg,
+          statusCode: 400,
+        });
+      }
+
+      next();
+    },
   },
 };
