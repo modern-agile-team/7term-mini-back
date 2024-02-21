11f5070fc43c618afee68ae481b80fd6f5f3eac2
jinwoo0207 <a91287840@gmail.com>
2024-02-21 15:46:21 +0900

    refactor/#4: app.js, userRoutes.js 경로 및 아이디 비밀번호 검사 수정

--- app/app.js
+++ app/app.js
@@ -21,7 +21,7 @@ app.use(
 
 app.use(express.json({ extended: true }));
 
-app.use("/user", user);
+app.use("/users", user);
 app.use("/auth", auth);
 app.use("/comment", comment);
 app.use("/loves", love);
--- app/src/models/user/userService.js
+++ app/src/models/user/userService.js
@@ -13,7 +13,8 @@ class UserService {
   async deleteUser() {
     const clientInfo = this.user;
     const deleteUserResult = await UserRepository.delete(clientInfo.no);
-    if (!deleteUserResult[0].affectedRows) {
+    console.log(deleteUserResult[0])
+    if (!deleteUserResult[0]) {
       return { error: "Internal Server Error", message: "회원탈퇴에 실패하였습니다.", statuscode: 500 };
     }
     return { statusCode: 201, message: "회원탈퇴에 성공하였습니다." };
