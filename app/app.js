cbbb9f2ec056b9fc1d88940659d7c53aa6ebdd65
cyoure <wwj9559@gmail.com>
2024-02-23 13:51:52 +0900

    refactor/#29: mypage user에 합치기

--- app/app.js
+++ app/app.js
@@ -5,7 +5,6 @@ import cors from "cors";
 import user from "./src/routes/user/userRoutes.js";
 import auth from "./src/routes/auth/authRoutes.js";
 import board from "./src/routes/board/boardRoutes.js";
-import mypage from "./src/routes/mypage/mypageRoutes.js";
 
 const app = express();
 
@@ -22,6 +21,5 @@ app.use(express.json({extended: true}));
 app.use("/users", user);
 app.use("/auth", auth);
 app.use("/boards", board);
-app.use("/mypage", mypage);
 
 export default app;
--- app/src/models/user/userService.js
+++ app/src/models/user/userService.js
@@ -14,9 +14,13 @@ class UserService {
     const clientInfo = this.user;
     const deleteUserResult = await UserRepository.delete(clientInfo.no);
     if (!deleteUserResult[0]) {
-      return { error: "Internal Server Error", message: "회원탈퇴에 실패하였습니다.", statuscode: 500 };
+      return {
+        error: "Internal Server Error",
+        message: "회원탈퇴에 실패하였습니다.",
+        statuscode: 500,
+      };
     }
-    return { statusCode: 201, message: "회원탈퇴에 성공하였습니다." };
+    return {statusCode: 201, message: "회원탈퇴에 성공하였습니다."};
   }
 
   async createUser() {
@@ -54,7 +58,7 @@ class UserService {
         hashPassword,
         clientInfo.email
       );
-      return { statusCode: 201, message: "회원가입에 성공하였습니다." };
+      return {statusCode: 201, message: "회원가입에 성공하였습니다."};
     } catch (err) {
       return {
         error: "Internal Server Error",
@@ -62,6 +66,25 @@ class UserService {
         statusCode: 500,
       };
     }
+  }
+  async findUserInformation() {
+    const userNo = this.user.no;
+    const userInformation = await UserRepository.findOneUser(userNo);
+    if (!userInformation[0][0]) {
+      return {
+        error: "Not Found",
+        message: "해당 유저가 존재하지 않습니다.",
+        statuscode: 404,
+      };
+    }
+    const [rows, field] = await UserRepository.findOneUser(userNo);
+
+    return {
+      statuscode: 200,
+      nickname: rows[0].nickname,
+      email: rows[0].email,
+      created_at: rows[0].created_at,
+    };
   }
 }
 export default UserService;
